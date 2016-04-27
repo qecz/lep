7ce33a3d1df23cf301d2c1d6f5fc10375339fd7f
alceo <alceo@alceo.ru>
2016-04-27 17:51:01 +0300

    init db

--- app.rb
+++ app.rb
@@ -2,6 +2,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db=SQLite3::Database.new  'lep.db'
+end
+
+before do
+	init_db
+end
+
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -10,3 +20,12 @@ end
 get '/new' do
   erb :new
 end
+
+post '/new' do
+  
+content = params[:content]
+
+erb "You typped #{content}" 
+
+end
+
